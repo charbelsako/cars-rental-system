e537f9ab4ccfba070a4de09cee40efdf7c9fd57b
charbelsako <charbelsako@gmail.com>
2021-10-14 10:42:36 +0300

    Fixed the code based on feedback

--- src/App.js
+++ src/App.js
@@ -1,69 +1,75 @@
 import { useState } from "react";
 import CardView from "./components/CardView";
+import Header from './components/Header';
 
 function App() {
-  const [cars] = useState([
+  const cars = [
     {
       title: "Honda",
       description: "really cool",
       image: "https://s3.caradvice.com.au/wp-content/uploads/2013/12/honda-city.jpg",
+      rentalNumber: 100,
     },
     {
       title: "Hyundai",
       description: "really cool",
       image:
         "http://www.zastavki.com/pictures/originals/2014/Auto___Hyundai__new_car_Hyundai_IX35__060506_.jpg",
+      rentalNumber: 108,
+
     },
     {
       title: "Kia",
       description: "really cool",
       image:
         "https://wallpapershome.com/images/wallpapers/kia-proceed-5120x2880-electric-car-5k-15722.jpg",
+      rentalNumber: 200,
+
     },
     {
       title: "Range Rover",
       description: "really cool",
       image:
         "https://media.autoexpress.co.uk/image/private/s--opXUk01t--/v1594728857/autoexpress/2020/07/Range%20Rover%20Sport%202021%20model%20year%20updates-8.jpg",
+      rentalNumber: 100,
+
     },
     {
       title: "Hummer",
       description: "really cool",
       image: "https://wallpapercave.com/wp/wc1754622.jpg",
+      rentalNumber: 100,
+
     },
     {
       title: "Mitsubishi",
       description: "really cool",
       image:
         "http://2.bp.blogspot.com/-VDnGW0s3Bps/T9dP7TqTsuI/AAAAAAAAEEg/GmayAp-CBFA/s1600/car-Mitsubishi-wallpapers.jpg",
+      rentalNumber: 100,
     },
     {
       title: "Mercedes",
       description: "really cool",
       image:
         "https://www.publicdomainpictures.net/pictures/260000/velka/mercedes-benz-sports-car.jpg",
+      rentalNumber: 100,
     },
     {
       title: "BMW",
       description: "really cool",
       image:
         "https://hdwallpaperim.com/wp-content/uploads/2017/08/23/472083-BMW-car.jpg",
+      rentalNumber: 100,
     },
-  ]);
+  ];
 
-  const [showRentals, setShowRentals] = useState(true);
-  const [rentalNumber, setRentalNumber] = useState(182);
+  const rentalNumber = 182;
 
-  const toggleRentalNumber = () => {
-    setShowRentals(!showRentals);
-  };
 
   return (
     <div className="App text-center">
-      <button onClick={toggleRentalNumber} className="btn btn-primary m-3">
-        {showRentals ? "Hide" : "Show"} Rentals
-      </button>
-      {showRentals ? <p>Number of rentals until today: {rentalNumber}</p> : null}
+      <Header />
       <CardView cars={cars} />
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import "../assets/Card.css";
 
-const Card = props => {
+const Card = (props) => {
+  const {
+    data: { image, title, description, rentalNumber },
+  } = props;
+
+  const [showRentals, setShowRentals] = useState(true);
+
+  const toggleRentalNumber = () => {
+    setShowRentals((prevState) => !prevState);
+  };
+
   return (
     <div className="">
-      <img
-        src={props.data.image}
-        alt=""
-        className="card-img-top"
-        style={{ width: "300px", margin: "0px auto" }}
-      />
+      <button onClick={toggleRentalNumber} className="btn btn-primary m-3">
+        {showRentals ? "Hide" : "Show"} Rentals
+      </button>
+      {showRentals ? (
+        <p>Number of rentals until today: {rentalNumber}</p>
+      ) : null}
+
+      <img src={image} alt="" className="card-img-top image" />
       <div className="card-body">
-        <h4 className="card-title">{props.data.title}</h4>
-        <p className="card-text">{props.data.description}</p>
+        <h4 className="card-title">{title}</h4>
+        <p className="card-text">{description}</p>
       </div>
     </div>
   );
